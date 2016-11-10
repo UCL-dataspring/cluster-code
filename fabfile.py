08d8bfd0a6cf37f7e4408a9475b38d6747c0cfeb
James Hetherington <jamespjh@gmail.com>
2016-11-10 20:48:57 +0000

    Correct path to data model for deploy

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@ env.user='ucgajhe'
 
 env.results_dir="/home/"+env.user+"/Scratch/BluclobberSpark/output"
 
-env.model="newsrods"
+env.model="bluclobber"
 env.corpus='/rdZone/live/rd003v/CompressedALTO-fromJamesH/CompressedALTO'
 
 env.deploy_to="/home/"+env.user+"/devel/BluclobberSpark"
