aa4864414ec15019919160a89bd774a8a8187b8d
James Hetherington <jamespjh@gmail.com>
2016-11-10 20:21:04 +0000

    Correct corpus path

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,7 @@ env.user='ucgajhe'
 env.results_dir="/home/"+env.user+"/Scratch/BluclobberSpark/output"
 
 env.model="newsrods"
-env.corpus='/rdZone/live/rd003v/CompressedALTO/'
+env.corpus='/rdZone/live/rd003v/CompressedALTO-fromJamesH/CompressedALTO'
 
 env.deploy_to="/home/"+env.user+"/devel/BluclobberSpark"
 env.clone_url="git@github.com:UCL/i_newspaper_rods.git"
